074c958c04d4cddf2246a7b398aa103efadb1709
Bradford Cross <bradford.n.cross@gmail.com>
2010-10-08 23:15:41 -0700

    adding clojars plugin.

--- project.clj
+++ project.clj
@@ -10,5 +10,6 @@
                  [rome "0.9"]
                  [clj-http "0.1.0-SNAPSHOT"]
                  [work "0.0.1-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]
+		     [lein-clojars "0.5.0"]]
   :repositories  {"apache" "https://repository.apache.org/content/repositories/releases/"})
