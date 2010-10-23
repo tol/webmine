70ab94abc079d1151466635973efaf2ff60da3bc
Bradford Cross <bradford.n.cross@gmail.com>
2010-10-22 19:15:04 -0700

    getting image sizes via fetch when they were not in the dom.

--- src/webmine/images.clj
+++ src/webmine/images.clj
@@ -1,4 +1,6 @@
 (ns webmine.images
+  (:import java.awt.Toolkit)
+  (:import javax.imageio.ImageIO)
   (:use
    infer.core
    webmine.urls
@@ -25,6 +27,7 @@
 
 (defn img-urls [us] (filter img? us))
 
+;;http://www.w3schools.com/tags/tag_IMG.asp
 (defn size [n]
   (if-let [attrs (.getAttributes n)]
     (let [w (.getNamedItem attrs "width")
@@ -43,4 +46,25 @@
   (let [is (filter :size images)]
 	(max-by #(apply * (:size %)) is)))
 
+;;example usage
 ;;(big-img (imgs (dom (:body (cl/get "http://gigaom.com/2010/10/22/whos-driving-mobile-payments-hint-some-are-barely-old-enough-to-drive/")))))
+
+(defn fetch-img [u]
+  (ImageIO/read (url u)))
+
+(defn img-size [u]
+  (let [i (fetch-img u)]
+    [(.getHeight i)
+     (.getWidth i)]))
+
+(defn fetch-sizes [imgs]
+  (map 
+   (fn [i]
+     (if-let [s (:size i)]
+       i
+       (let [s (img-size (:url i))]
+	 (assoc i :size s))))     
+   imgs))
+
+;;example usage
+;;(big-img (fetch-sizes (imgs (dom (:body (cl/get "http://gigaom.com/2010/10/22/whos-driving-mobile-payments-hint-some-are-barely-old-enough-to-drive/"))))))
--- test/webmine/images_test.clj
+++ test/webmine/images_test.clj
@@ -5,4 +5,4 @@
 (deftest extract-hw-test
   (let [s "height: 500px; width: 376px;"]
     (is (= [500 376]
-	   (extract-hw s)))))
+	   (ints (extract-hw s))))))
